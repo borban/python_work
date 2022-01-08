ecd3cd6e979b7b409386423993ddbae692d51815
borban <borban@@users.noreply.github.com>
2022-01-08 10:55:40 -0600

    Chapter 2 Strings

---
+++ simple_message_21.py
@@ -0,0 +1,2 @@
+simple_message = "This is pretty simple"
+print(simple_message)
---
+++ simple_message_22.py
@@ -0,0 +1,4 @@
+simple_message = "This is pretty simple"
+print(simple_message)
+simple_message = "Still pretty simple"
+print(simple_message)
