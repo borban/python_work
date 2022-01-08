057141ac3fbc857810ace541321bbcc5b738f527
borban <borban@@users.noreply.github.com>
2022-01-08 15:36:51 -0600

    Adding comment

--- hello_world.py
+++ hello_world.py
@@ -1,1 +1,9 @@
-print("Hello Python world!")
+# This is a comment about my Hello Python World code
+message = "Hello Python world!"
+print(message)
+
+message = "Hello Python Crash course world!"
+print(message)
+
+mesage = "Hello Python Crash course reader!"
+print(mesage)
