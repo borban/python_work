5ff23d79b082dfa9538a2933bb88fba00bef02f8
borban <borban@@users.noreply.github.com>
2022-01-17 13:27:52 -0600

    Coursework

---
+++ module_discussion3.py
@@ -0,0 +1,20 @@
+favorites_list = []
+
+favorites_list.append("Coca Cola")
+favorites_list.append("Pizza")
+favorites_list.append("Rock")
+
+print("My favorite drink is " + favorite_item)
+print("My favorite food is " + favorite_item)
+print("My favorite music is " + favorite_item)
+
+
+favorites_list = []
+
+favorites_list.append("Coca Cola")
+favorites_list.append("Pizza")
+favorites_list.append("Rock")
+
+print(f"My favorite food is {favorites_list [0]}, my favorite music is {favorites_list [1]}, my favorite drink is {favorites_list [2]},")
+
+
